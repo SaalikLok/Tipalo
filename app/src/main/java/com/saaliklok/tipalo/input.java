ef53b394958bc13591fa16e7dfef03eaea409f5e
Saalik Lokhandwala <saaliklok@Saaliks-MacBook-Air.local>
2017-02-12 17:01:04 -0500

    Added calculations and logs, broken.

--- app/src/main/java/com/saaliklok/tipalo/input.java
+++ app/src/main/java/com/saaliklok/tipalo/input.java
@@ -1,8 +1,13 @@
 package com.saaliklok.tipalo;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class input extends AppCompatActivity {
 
@@ -19,29 +24,47 @@ public class input extends AppCompatActivity {
     EditText people = (EditText) findViewById(R.id.numberPeople);
     EditText tipPercent = (EditText) findViewById(R.id.tipPercent);
 
+    //The Text Views
+    TextView title = (TextView) findViewById(R.id.titleView);
+    TextView billQuestion = (TextView) findViewById(R.id.billQuestion);
+    TextView splitBetween = (TextView) findViewById(R.id.splitBetween);
+    TextView loveWaiter = (TextView) findViewById(R.id.loveWaiter);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
 
+        //Define the custom font and set all of the text views to that font
+        Typeface raleway = Typeface.createFromAsset(getAssets(), "fonts/Raleway-Regular.ttf");
+        title.setTypeface(raleway);
+        billQuestion.setTypeface(raleway);
+        splitBetween.setTypeface(raleway);
+        loveWaiter.setTypeface(raleway);
 
-    }
+        Button submitButton = (Button) findViewById(R.id.button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Getting all the EditText Values as Strings
+                String strInitBill = initBill.getText().toString();
+                String strPeople = people.getText().toString();
+                String strtipPercent = tipPercent.getText().toString();
 
-    public void calculateCheck(){
-        //Getting all the EditText Values as Strings
-        String strInitBill = initBill.getText().toString();
-        String strPeople = people.getText().toString();
-        String strtipPercent = tipPercent.getText().toString();
+                //Convert all of the inputs that are strings into doubles
+                numinitBill = Double.valueOf(strInitBill);
+                numPeople = Double.valueOf(strPeople);
+                numtipPercent = Double.valueOf(strtipPercent);
 
-        //Convert all of the inputs that are strings into doubles
-        numinitBill = Double.valueOf(strInitBill);
-        numPeople = Double.valueOf(strPeople);
-        numtipPercent = Double.valueOf(strtipPercent);
+                //Calculate all of the final numbers the user needs
+                tipAmount = numinitBill * numtipPercent;
+                totalBill = numinitBill + tipAmount;
+                billPerPerson = totalBill/numPeople;
+                tipPerPerson = tipAmount/numPeople;
 
-        tipAmount = numinitBill * numtipPercent;
-        totalBill = numinitBill + tipAmount;
-        billPerPerson = totalBill/numPeople;
-        tipPerPerson = tipAmount/numPeople;
-
+                Log.i("TotalBill", "Total Bill:" + totalBill);
+                Log.i("BillPer", "Bill Per Person:" + billPerPerson);
+            }
+        });
     }
 }
